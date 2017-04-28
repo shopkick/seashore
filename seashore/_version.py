35e3b1f1d188acc9dab65f07745611feacf77598
Moshe Zadka <moshe@shopkick.com>
2017-04-28 12:51:26 -0700

    version

---
+++ seashore/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides seashore version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update seashore` to change this file.
+
+from incremental import Version
+
+__version__ = Version('seashore', 17, 4, 0)
+__all__ = ["__version__"]
