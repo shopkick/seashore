46b4279833d9fc9dbc0d610feef91423827678c6
kurt.rose <kurt.rose@shopkick.com>
2017-05-11 15:14:53 -0700

    added basepython thing to get tox.ini working

--- test/manylinux/run-docker-test.py
+++ test/manylinux/run-docker-test.py
@@ -13,8 +13,10 @@ tag = 'seashore-test-' + os.environ['CI_JOB_ID']
 dst = docker_ctx + '/repo'
 src = os.environ['CI_PROJECT_DIR']
 os.mkdir(dst)
-subprocess.check_call(['cp', '-r', src + '/src', dst])
-subprocess.check_call(['cp', '-r', src + '/tox.ini', dst])
+for p in os.listdir(src):
+   if p == 'test':
+       continue
+   subprocess.check_call(['cp', '-r', src + '/' + p, dst])
 subprocess.check_call(['docker', 'build', '-t', tag, docker_ctx])
 subprocess.check_call(['docker', 'rmi', tag])
 
