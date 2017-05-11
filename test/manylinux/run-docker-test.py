a860d3002ae2889b8117056035f457a108c76536
kurt.rose <kurt.rose@shopkick.com>
2017-05-11 14:41:09 -0700

    trying a very different CI approach

---
+++ test/manylinux/run-docker-test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+if __name__ != "__main__":
+    raise ImportError()
+
+
+import subprocess
+import os
+
+
+tag = 'seashore-test-' + os.environ['CI_JOB_ID']
+subprocess.check_call(['cp', '-r', os.environ['OS_PROJECT_DIR'], '.'])
+subprocess.check_call(['docker', 'build', '-t', tag, '.'])
+subprocess.check_call(['docker', 'rmi', tag])
+
