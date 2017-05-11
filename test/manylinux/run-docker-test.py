b3cc683cb735b8afccb7421664f5d33e6941466f
kurt.rose <kurt.rose@shopkick.com>
2017-05-11 14:49:52 -0700

    fixing CI

--- test/manylinux/run-docker-test.py
+++ test/manylinux/run-docker-test.py
@@ -5,10 +5,16 @@ if __name__ != "__main__":
 
 import subprocess
 import os
+import os.path
 
 
+docker_ctx = os.path.dirname(os.path.abspath(__file__))
 tag = 'seashore-test-' + os.environ['CI_JOB_ID']
-subprocess.check_call(['cp', '-r', os.environ['CI_PROJECT_DIR'], '.'])
+dst = docker_ctx + '/repo'
+src = os.environ['CI_PROJECT_DIR']
+os.mkdir(dst)
+subprocess.check_call(['cp', '-r', src + '/src', dst])
+subprocess.check_call(['cp', '-r', src + '/tox.ini', dst])
 subprocess.check_call(['docker', 'build', '-t', tag, '.'])
 subprocess.check_call(['docker', 'rmi', tag])
 
