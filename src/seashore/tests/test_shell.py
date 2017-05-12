cfdd8b769135f7fb8a2e7cb651549f0b988bdbf5
Moshe Zadka <moshe@shopkick.com>
2017-05-11 17:16:15 -0700

    py3

--- src/seashore/tests/test_shell.py
+++ src/seashore/tests/test_shell.py
@@ -36,7 +36,7 @@ class ShellTest(unittest.TestCase):
 
     def test_env(self):
         self.shell.setenv(b'SPECIAL', b'emett')
-        python_script = b'import sys,os;sys.stdout.write(os.environ[b"SPECIAL"])'
+        python_script = b'import sys,os;sys.stdout.write(str(os.environ[b"SPECIAL"]))'
         out, _ignored = self.shell.batch([sys.executable, b'-c', python_script])
         self.assertEquals(out, b'emett')
         self.assertEquals(self.shell.getenv(b'SPECIAL'), b'emett')
