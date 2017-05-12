b01916f8ef7e7781a3a5baa2204557740661b09f
Moshe Zadka <moshe@shopkick.com>
2017-05-11 17:50:23 -0700

    py3

--- src/seashore/tests/test_shell.py
+++ src/seashore/tests/test_shell.py
@@ -36,7 +36,7 @@ class ShellTest(unittest.TestCase):
 
     def test_env(self):
         self.shell.setenv('SPECIAL', b'emett')
-        python_script = b'import sys,os;sys.stdout.write(os.environ["SPECIAL"].encode("ascii"))'
+        python_script = b'import sys,os;sys.stdout.write(os.environ["SPECIAL"].decode("ascii"))'
         out, _ignored = self.shell.batch([sys.executable, b'-c', python_script])
         self.assertEquals(out, b'emett')
         self.assertEquals(self.shell.getenv(b'SPECIAL'), b'emett')
@@ -57,7 +57,7 @@ class ShellTest(unittest.TestCase):
         new_shell = self.shell.clone()
         new_shell.setenv('SPECIAL', b'lucy')
         self.shell.setenv('SPECIAL', b'emett')
-        python_script = b'import sys,os;sys.stdout.write(os.environ["SPECIAL"].encode("ascii"))'
+        python_script = b'import sys,os;sys.stdout.write(os.environ["SPECIAL"].decode("ascii"))'
         out, _ignored = new_shell.batch([sys.executable, '-c', python_script])
         self.assertEquals(out, b'lucy')
 
